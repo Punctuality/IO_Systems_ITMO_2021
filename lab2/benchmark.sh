1ff57ce07afdadd5b6211d4be2da7e7efee96938
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-04-02 12:59:19 +0300

    Updated benchmark script to fish shell

--- lab2/benchmark.sh
+++ lab2/benchmark.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/fish
 
-if [ "$(whoami)" != "root" ]; then
-  sudo "$0" "$@"
-  exit $?
-fi
+if [ (whoami) != 'root' ]
+  sudo './'(basename (status -f)) $argv
+  exit $status
+end
 
 mkfs.vfat /dev/mydisk1
 mkfs.vfat /dev/mydisk2
@@ -20,19 +20,19 @@ mount /dev/mydisk2 /mnt/disk2
 mount /dev/mydisk5 /mnt/disk5
 mount /dev/mydisk6 /mnt/disk6
 
-function create_files() {
+function create_files
   dd if=/dev/urandom of=/mnt/disk1/file bs=1M count=5
   dd if=/dev/urandom of=/mnt/disk2/file bs=1M count=5
   dd if=/dev/urandom of=/mnt/disk5/file bs=1M count=5
   dd if=/dev/urandom of=/mnt/disk6/file bs=1M count=5
-}
+end
 
-function delete_files() {
+function delete_files
   rm /mnt/disk1/file
   rm /mnt/disk2/file
   rm /mnt/disk5/file
   rm /mnt/disk6/file
-}
+end
 
 create_files
 
@@ -57,3 +57,9 @@ umount /dev/mydisk1
 umount /dev/mydisk2
 umount /dev/mydisk5
 umount /dev/mydisk6
+
+rm -rf /tmp/io/
+rmdir /mnt/disk1
+rmdir /mnt/disk2
+rmdir /mnt/disk5
+rmdir /mnt/disk6
